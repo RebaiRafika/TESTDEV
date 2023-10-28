3bebdb37aeea6b04f0a93c707d439c55650435ca
RebaiRafika <147846850+RebaiRafika@users.noreply.github.com>
2023-10-28 13:56:27 +0100

    Add files via upload

---
+++ Rafika_Rebai_LSI3_DevMobil_Mesure_Glycemie/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Rafika_Rebai_LSI3_DevMobil_Mesure_Glycemie"
+include(":app")
+ 
---
+++ Rafika_Rebai_LSI3_DevMobil_Mesure_Glycemie_TP2/app/src/main/java/com/example/rafika_rebai_lsi3_devmobil_mesure_glycemie_tp2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rafika_rebai_lsi3_devmobil_mesure_glycemie_tp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
